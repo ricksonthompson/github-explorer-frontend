3a42a711ba95cd1a29d1d58d649d9ec9f7a660be
Rickson Thompson <ricksonthompson.dev@gmail.com>
2021-03-31 02:22:18 -0400

    finalizada a estilização da dashboard

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -7,6 +7,7 @@ import githubLogo from '../../assets/logo.svg';
 // Dessa forma é declarado o componente em formato de função
 const Dashboard: React.FC = () => {
   return (
+    // <> </> fragments são utlizados quando há mais de um elemento declarado
     <>
       <img src={githubLogo} alt="GitHub Explorer" />
       <Title>Explore repositórios no GitHub</Title>
@@ -18,19 +19,25 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         <a href="teste">
-          <img src="https://github.com/danilo-vieira.png" alt="Danilo Vieira" />
+          <img
+            src="https://avatars.githubusercontent.com/u/41199059?v=4"
+            alt="Rickson Thompson"
+          />
           <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+            <strong>github-explorer-backend</strong>
+            <p>Aplicação desenvolvida em ReactJS</p>
           </div>
 
           <FiChevronRight size={20} />
         </a>
         <a href="teste">
-          <img src="https://github.com/danilo-vieira.png" alt="Danilo Vieira" />
+          <img
+            src="https://avatars.githubusercontent.com/u/56176702?v=4"
+            alt="William Sampaio"
+          />
           <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+            <strong>jogo-da-velha-C</strong>
+            <p>Jogo da velha desenvolvido em linguagem C</p>
           </div>
 
           <FiChevronRight size={20} />
--- frontend/src/pages/Dashboard/styles.tsx
+++ frontend/src/pages/Dashboard/styles.tsx
@@ -6,20 +6,27 @@ export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
   max-width: 450px;
+  // vai forçar o texto a quebrar
   line-height: 56px;
   margin-top: 80px;
+  // vai distanciar o topo da logo
 `;
 
 export const Form = styled.form`
+  // O styled permite trabalhar com encadeamento, ou seja, tudo aqui dentro vai receber essas configs
+
   margin-top: 40px;
   max-width: 700px;
+  // vai ocupar o espaço disponível
   display: flex;
+  // vai ser flexivel internamente
   input {
     flex: 1;
     height: 70px;
     padding: 0 24px;
     border: 0;
     border-radius: 5px 0 0 5px;
+    // define o posicionamento do placeholder
     color: #3a3a3a;
     &::placeholder {
       color: #a8a8b3;
@@ -34,8 +41,10 @@ export const Form = styled.form`
     color: #fff;
     font-weight: bold;
     transition: background-color 0.2s;
+    // define a velocidade do hover
     &:hover {
       background: ${shade(0.2, '#04d361')};
+      // Utiliza a lib polished pra aplicar a opacidade
     }
   }
 `;
@@ -50,19 +59,27 @@ export const Repositories = styled.div`
     padding: 24px;
     display: block;
     text-decoration: none;
+    // Remove os underlines por volta
     display: flex;
+    // Para os elementos ficarem ao lado do outro
     align-items: center;
+    // Alinhar os eixos no centro
     transition: transform 0.2s;
     & + a {
+      // & referencia um elemento apenas. Como um this
+      // + quer dizer que, se ele elemento prodeceder de um semelhante, ele vai aplicar a estilização
       margin-top: 16px;
+      // Aplica o distanciamento entre os objetos
     }
     &:hover {
       transform: translateX(10px);
+      // Permite fazer essa transição pro lado
     }
     img {
       width: 64px;
       height: 64px;
       border-radius: 50%;
+      // Faz a borda ficar arredondada
     }
     div {
       margin-left: 16px;
@@ -77,7 +94,9 @@ export const Repositories = styled.div`
       }
     }
     svg {
+      // Permite manipular os icons
       margin-left: auto;
+      // Pega toda a margem da esquerda e joga pra direita
       color: #cbcbd6;
     }
   }
